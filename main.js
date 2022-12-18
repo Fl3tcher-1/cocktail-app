c6b7161c7173db24b5bf58871cfef8997dc77728
Fl3tcher-1 <karolis.milius7@gmail.com>
2022-12-18 16:24:30 +0000

    final layout with blur effects

--- main.js
+++ main.js
@@ -16,36 +16,50 @@ data.drinks.forEach(element => {
 }
 
 function makeDivs(drink){
-
+    let listItems =[]
     let container = document.getElementById('grid')
-    let img = document.getElementById('image')
-    let name = document.getElementById('header')
-    let list = document.getElementById('list')
-    let cocktailName 
-    
-    //create a div for every drink property
     let div = document.createElement('div')
+    div.className = "gridCocktails"
+    
+    // let listItem = document.createElement('li')
+    let list = document.createElement('ul')
     
     drink.forEach(property =>{
+        let h = document.createElement('h2')
+        let img = document.createElement('img')
+        list.className = "list"
         
         if(property[0]=="strDrink"){
+            h.className ="header"
+            h.innerHTML = property[1]
             name.innerHTML = property[1]
+            div.appendChild(h)
         } 
         if(property[0] =="strDrinkThumb"){
+            img.className = "image"
             img.setAttribute("src", `${property[1]}`)
+            div.appendChild(img)
         } else{
             //checks for categories to ignore
-            let listItem = document.createElement('li')
             if(property[0] !=="strInstructionsDE" && property[0] !== "strInstructionsIE" && property[0]!="strInstructionsIT" && property[0]!= "idDrink" && property[0]!= "strDrink"
-             && property[0]!= "strImageSource" && property[0] != "strImageAttribution" && property[0] != "strCreativeCommonsConfirmed" && property[0]!= "dateModified" && property[0] != "strVideo"){
-                // div.textContent = `${property[0]} :  ${property[1]}`
-                listItem.textContent = `${property[0].replace("str", "")} :  ${property[1]}`
-                // div.appendChild(ul)
-                list.appendChild(listItem)
+            && property[0]!= "strImageSource" && property[0] != "strImageAttribution" && property[0] != "strCreativeCommonsConfirmed" && property[0]!= "dateModified" && property[0] != "strVideo"){
+                listItems.push(`${property[0].replace("str", "")} :<br>  ${property[1]}`)
             }
 
         }
+
+        
     })
+    
+    // joins array items into list elements
+    var htmlList = listItems.map(item =>{
+        return '<li>' +item + '</li>'
+    }).join('')
+
+    // console.log(htmlList)
+    list.innerHTML = htmlList
+
+    div.appendChild(list)
 
     // drink.forEach(property => {
     //     div.className = 'cocktail'
@@ -87,7 +101,7 @@ function makeDivs(drink){
 function getCockatils (cocktail){
 
     // if there are any existing cocktails-- remove them before sending a fetch request
-    let cocktails = document.getElementsByClassName('cocktail')
+    let cocktails = document.getElementsByClassName('gridCocktails')
     if( cocktails.length !==0){
         console.log(cocktails.item(0))
         for(let i =0; i <cocktails.length;){
@@ -106,8 +120,35 @@ function getCockatils (cocktail){
     
     .catch(error => console.error(error))
 
-    
-
-
 }
+
+
+// *********************** SCROLL BLUR*****************
+
+var timer
+
+// when the user scrolls it blurs the screen, on 100ms of scroll release it unblurs
+window.addEventListener("scroll", (e)=>{
+    clearTimeout(timer)
+
+    var elements = document.getElementById('gridContainer')
+
+    // console.log(elements.children['grid'].children)
+
+    for(let i =0; i <elements.children['grid'].children.length; i ++){
+        elements.children['grid'].children[i].children[2].style.filter = 'blur(3px)'
+    }
+    
+    // elements.style.height = "100px"
+    // elements.style.filter = 'blur(3px)'
+
+    timer = setTimeout( function(){
+        for(let i =0; i <elements.children['grid'].children.length; i ++){
+            elements.children['grid'].children[i].children[2].style.filter = 'blur(0px)'
+        }
+        // elements.style.filter = 'blur(0px)'
+    },100)
+
+   
+})
 
